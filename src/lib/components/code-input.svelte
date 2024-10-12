75445601aa9b12a32ee862e642de9ee1c8c03452
Nicola Clark <nicola@slottedspoon.dev>
2024-10-12 11:16:31 -0500

    adjust code input styles to prevent zooming on mobile

--- src/lib/components/code-input.svelte
+++ src/lib/components/code-input.svelte
@@ -9,10 +9,18 @@
     border: 1px solid @color-dark;
     border-radius: @border-r-xl;
     box-sizing: border-box;
-    height: .full-without-padding(@padding-xl-y) [];
-    margin: @padding-xl;
+    font-size: @font-s-md;
+    height: .full-without-padding(@padding-lg-y) [];
+    margin: @padding-lg;
     padding: @padding-lg;
     resize: none;
-    width: .full-without-padding(@padding-xl-x) [];
+    width: .full-without-padding(@padding-lg-x) [];
+
+    @media screen and (min-width: @sizes[lg]) {
+      font-size: unset;
+      height: .full-without-padding(@padding-xl-y) [];
+      margin: @padding-xl;
+      width: .full-without-padding(@padding-xl-x) [];
+    }
   }
 </style>
