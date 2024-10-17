23655f71d31f79024aa15f131abcf7d853f82547
Nicola Clark <nicola@slottedspoon.dev>
2024-10-17 11:49:22 -0500

    add tests for preview

--- tests/general.test.ts
+++ tests/general.test.ts
@@ -13,3 +13,23 @@ test('nav items work', async ({ page }) => {
     await expect(page.getByTestId(`${await tab.textContent()}-pane`)).toBeVisible();
   }
 });
+
+test('preview accepts content', async ({ page }) => {
+  await page.goto('/');
+  await page.getByText('content').click();
+  await page.getByRole('textbox').fill('# résumé');
+
+  const previewFrame = page.getByTitle('résumé preview').contentFrame();
+  await expect(previewFrame.getByRole('heading')).toHaveText('résumé');
+});
+
+test('preview accepts styles', async ({ page }) => {
+  await page.goto('/');
+  await page.getByText('content').click();
+  await page.getByRole('textbox').fill('# blah');
+  await page.getByText('style', { exact: true }).click();
+  await page.getByRole('textbox').fill('h1 { color: red; }');
+
+  const previewFrame = page.getByTitle('résumé preview').contentFrame();
+  await expect(previewFrame.getByRole('heading')).toHaveCSS('color', 'rgb(255, 0, 0)');
+});
